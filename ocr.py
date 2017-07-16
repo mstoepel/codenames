057a721828a1c711b911b697ce11fee6211cc6a9
Michael <mstoepel@gmail.com>
2017-07-16 17:50:21 -0400

    added os joins

--- ocr.py
+++ ocr.py
@@ -1,6 +1,9 @@
 from subprocess import call
+import os
 
-call("python ..\ocropy-master\ocropus-nlbin -n Images\image1.* -o OCR_Images")
-call("python ..\ocropy-master\ocropus-gpageseg OCR_Images\*.bin.png")
-call("python ..\ocropy-master\ocropus-rpred -m ..\ocropy-master\models\uw3unlv-plus-00100000.pyrnn OCR_Images\*\*.bin.png")
-call("python ..\ocropy-master\ocropus-hocr *\*.bin.png -o words.html")
+base_path = os.path.join('..', 'ocropy-master')
+
+call("python {} -n Images\image1.* -o OCR_Images".format(os.path.join(base_path, 'ocropus-nlbin')), shell=True)
+call("python {} OCR_Images\*.bin.png".format(os.path.join(base_path, 'ocropus-gpageseg')), shell=True)
+call("python {} -m ..\ocropy-master\models\uw3unlv-plus-00100000.pyrnn OCR_Images\*\*.bin.png".format(os.path.join(base_path, 'ocropus-rpred')), shell=True)
+call("python {} *\*.bin.png -o words.html".format(os.path.join(base_path, 'ocropus-hocr')), shell=True)
